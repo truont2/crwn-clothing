17f2de1586749c1642e814f0791adde0dac2033b
Takara Truong <truont2@gmail.com>
2022-11-06 14:34:59 -0800

    created checkout page with checkout item and cart functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route} from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component.jsx";
 import Authentication from "./routes/authentication/authentication.component.jsx";
 import Shop from './routes/shop/shop.component.jsx'
+import Checkout from "./routes/checkout/checkout.component.jsx";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route index={true} element={<Home />} />  
         <Route path='auth' element={<Authentication />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,8 +4,18 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 import CartItem from '../cart-item/cart-item.component';
 
+import { useNavigate } from 'react-router-dom';
+
+
 const CartDropdown = () => {
     const {cartItems} = useContext(CartContext);
+
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () => {
+        navigate('/checkout');
+    }
+    
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
@@ -13,7 +23,7 @@ const CartDropdown = () => {
                     return <CartItem key={item.id} cartItem={item}/>
                 })}
             </div>
-            <Button>Go to Checkout</Button>
+            <Button onClick={goToCheckoutHandler}>Go to Checkout</Button>
         </div>
     )
 }
